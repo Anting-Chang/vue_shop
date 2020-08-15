cff097a8d6eb0b4dc6c85aa362c6f63b59aff504
anting chang <antingchang@yahoo.ca>
2020-08-15 17:19:37 +0800

    Create home structure and css

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="app">
-<!--    APP跟组件-->
+    <!--    APP跟组件-->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
 }
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">Exit</el-button>
-  </div>
+  <el-container class="home-container">
+    <!--      header -->
+    <el-header>
+      <div>
+        <img src="" alt="">
+        <div>Shop Manager</div>
+      </div>
+      <el-button class="logout-btn" type="info" @click="logout">Logout</el-button>
+    </el-header>
+    <!--      main container -->
+    <el-container>
+<!--      aside -->
+      <el-aside width="200px">
+        <el-col :span="12">
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            background-color="#333744"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item index="1-4-1">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>导航二</span>
+                </template>
+              </el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-col>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -17,5 +52,22 @@ export default {
 </script>
 
 <style scoped>
+  .logout-btn {
+    margin: 10px 0;
+  }
+  .home-container {
+    height: 100%;
+  }
+  .el-header {
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+  }
 
+  .el-aside {
+    background-color: #333744;
+  }
+  .el-main {
+    background-color: #eaedf1;
+  }
 </style>
